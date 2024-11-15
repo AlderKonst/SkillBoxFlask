0ec2da169b9cde8830e3220a548b7074fd1ce13b
Я <koalder@yandex.ru>
2024-11-15 13:04:54 +0300

    В app.py добавил возможность динамического создания страниц через адресную строку

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Привет, мир!'
+def index():
+    name = 'Саша'
+    return render_template('index.html',
+                           name=name)
+@app.route('/<name>/')
+def naming(name):
+    my_name = f'''
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Моё имя {name}</title>
+</head>
+<body>
+    <p>Моё имя {name}</p>
+</body>
+</html>
+'''
+    with open(f'templates/name_{name}.html', 'w', encoding='UTF-8') as f:
+        f.write(my_name)
+    return my_name
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
